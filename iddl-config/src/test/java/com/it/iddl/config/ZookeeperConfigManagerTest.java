da09967b27c196480605169feec7efe98db949cf
sihai <iac-rq@live.cn>
2012-07-20 03:04:45 +0800

    testcase

---
+++ iddl-config/src/test/java/com/it/iddl/config/ZookeeperConfigManagerTest.java
@@ -0,0 +1,41 @@
+/**
+ * iddl
+ * 
+ * Intelligent Distributed Data Layer
+ */
+package com.it.iddl.config;
+
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import com.it.iddl.config.factory.ConfigManagerFactory;
+import com.it.iddl.config.listener.AbstractConfigListener;
+
+/**
+ * 
+ * @author sihai
+ *
+ */
+public class ZookeeperConfigManagerTest extends TestCase {
+
+	private static final String APP_NAME = "testApp";
+	private static final String DB_KEY = "testDB";
+	
+	public void test() throws Exception {
+		Properties properties = new Properties();
+		ConfigManager configManger = ConfigManagerFactory.newZookeeperConfigManagerInstance(ZookeeperConfigManager.ZookeeperConfig.fromProperties(properties));
+		String configId = configManger.makeConfigId(APP_NAME, DB_KEY);
+		String value = configManger.getConfigValue(configId);
+		System.out.println(String.format("value:%s for configId:%s", value, configId));
+		configManger.getConfigValue(configId, new AbstractConfigListener() {
+
+			@Override
+			public void changed(String newValue) {
+				System.out.println(String.format("Config change, configId:%s, newValue:%s", getConfigId(), newValue));
+			}
+		});
+		
+		Thread.sleep(1000 * 60 * 5);
+	}
+}
